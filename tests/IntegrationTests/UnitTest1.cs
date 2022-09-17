dd632356f0a6078a89352fc859501bc605c86e2b
Mathias de Mestral <mathiasdemestral@gmail.com>
2022-09-17 11:24:29 -0400

    Implement test collection in UnitTest1.cs

--- tests/IntegrationTests/UnitTest1.cs
+++ tests/IntegrationTests/UnitTest1.cs
@@ -1,7 +1,15 @@
 namespace IntegrationTests;
 
+[Collection("Test collection")]
 public class UnitTest1
 {
+    private readonly SharedTestContext _testContext;
+
+    public UnitTest1(SharedTestContext testContext)
+    {
+        _testContext = testContext;
+    }
+
     [Fact]
     public void Test1()
     {
